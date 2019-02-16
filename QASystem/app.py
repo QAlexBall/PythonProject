e853c2e6ac10912fb776e67880618c8e8e4c2d3c
QAlexBall <zhuderen12345@icloud.com>
2019-02-16 22:00:33 +0800

    make login page.

--- QASystem/app.py
+++ QASystem/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import config
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ app.config.from_object(config)
 def index():
     return render_template('index.html')
 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
