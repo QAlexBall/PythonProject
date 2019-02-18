032fb7db91781b67d5ee8c1016ce3ee692af60fb
QAlexBall <zhuderen12345@icloud.com>
2019-02-18 23:12:57 +0800

    add register page.

--- QASystem/app.py
+++ QASystem/app.py
@@ -12,6 +12,15 @@ def index():
 def login():
     if request.method == 'GET':
         return render_template('login.html')
+    else:
+        pass
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        pass
 
 if __name__ == '__main__':
     app.run()
