7f1ff69ae0a662af752a960e3aad73249d580890
QAlexBall <zhuderen12345@icloud.com>
2019-02-19 12:09:56 +0800

    create user model

--- QASystem/manage.py
+++ QASystem/manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app
 from extensions import db
-
+from models import User
 
 manager = Manager(app)
 
