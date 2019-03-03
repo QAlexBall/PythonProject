171671b3950164f8a7ee58a71dc237d85ffdc9f6
QAlexBall <zhuderen12345@icloud.com>
2019-03-03 23:25:33 +0800

    update qasystem

--- QASystem/models.py
+++ QASystem/models.py
@@ -1,6 +1,7 @@
 from extensions import db
 
 class User(db.Model):
+
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     telephone = db.Column(db.String(11), nullable=False)
