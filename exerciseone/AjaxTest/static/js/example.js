f4f4df0cd4292bed80148a082ed9492272f9d230
qalexball <zhuderen12345@icloud.com>
2019-03-28 11:07:19 +0800

    update

--- exerciseone/AjaxTest/static/js/example.js
+++ exerciseone/AjaxTest/static/js/example.js
@@ -9,17 +9,24 @@ class LikeButton {
   constructor() {
     this.state = { isLiked: false }
   }
+
+  setState (state) {
+    const oldEl = this.el
+    this.state = state
+    this.el = this.render()
+    if (this.onStateChange) this.onStateChange(oldEl, this.el)
+  }
   
   changeLikeText() {
-    const likeText = this.el.querySelector('.like-text')
-    this.state.isLiked = !this.state.isLiked
-    likeText.innerHTML = this.state.isLiked ? '取消' : '点赞'
+    this.setState({
+      isLiked: !this.state.isLiked
+    })
   }
 
   render () {
     this.el = createDOMFromString(`
       <button id='like-btn'>
-        <span class='like-text'> 点赞 </span>
+        <span class='like-text'> ${ this.state.isLiked ? '取消' : '点赞' } </span>
         <span> 👍 </span>
       </button>
     `)
@@ -29,6 +36,14 @@ class LikeButton {
 }
 
 const wrapper = document.querySelector('.wrapper')
+const likeButton = new LikeButton()
+wrapper.appendChild(likeButton.render())
+likeButton.onStateChange = (oldEl, newEl) => {
+  wrapper.insertBefore(newEl, oldEl)
+  wrapper.removeChild(oldEl)
+}
+/*
+const wrapper = document.querySelector('.wrapper')
 const likeButton1 = new LikeButton()
 wrapper.appendChild(likeButton1.render())
 // wrapper.innerHTML = likeButton1.render()
@@ -37,4 +52,4 @@ const likeButton2 = new LikeButton()
 wrapper.appendChild(likeButton2.render())
 // wrapper.innerHTML += likeButton2.render()
 
-
+*/
